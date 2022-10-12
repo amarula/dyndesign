de82b43bc40435a6a3f542df1779eb5447a7b2b7
Patrizio Gelosi <patrizio.gelosi@amarulasolutions.com>
2022-10-12 09:43:50 +0200

    DynLoader: change behavior in 1-arg usage
    If `import_class` is invoked with 1 argument only, that argument is
    interpreted as a string formatted as "module_name.class_name".
    
    Signed-off-by: Patrizio Gelosi <patrizio.gelosi@amarulasolutions.com>

--- dyndee/dyn_loader.py
+++ dyndee/dyn_loader.py
@@ -1,7 +1,5 @@
 from typing import Type
 
-from utils.string import StringUtils
-
 
 class DynLoader:
     """Toolset for class dynamic loading."""
@@ -14,6 +12,7 @@ class DynLoader:
         :param class_name: name of the class in the module to import, if different from the module's name.
         :return: dynamically imported class.
         """
-        if not class_name: class_name = StringUtils.get_last_dot_chunk(module_name)
+        if not class_name:
+            module_name, class_name = module_name.rsplit('.', 1)
         loaded_module = __import__(module_name, fromlist=class_name)
         return getattr(loaded_module, class_name)
--- tests/test_dyn_loader.py
+++ tests/test_dyn_loader.py
@@ -2,7 +2,7 @@ from dyndee.dyn_loader import DynLoader
 
 TEST_CLASS_1_MODULE = 'tests.sample_classes'
 TEST_CLASS_1_NAME = 'A'
-TEST_CLASS_2_MODULE = 'tests.DummyClass'
+TEST_CLASS_2_MODULE_CLASS = 'tests.DummyClass.DummyClass'
 TEST_CLASS_2_NAME = 'DummyClass'
 
 
@@ -14,5 +14,5 @@ def test_import_class_1():
 
 def test_import_class_2():
     """A test class #2 is dynamically imported from a module with the same name of the class."""
-    class_2 = DynLoader.import_class(TEST_CLASS_2_MODULE)
+    class_2 = DynLoader.import_class(TEST_CLASS_2_MODULE_CLASS)
     assert class_2.__name__ == TEST_CLASS_2_NAME, "Error importing class #2"
